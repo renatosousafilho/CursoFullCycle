465f4ea080cf3785581f70e07693787db5ab7025
Renato Filho <renatosousafilho@gmail.com>
2024-01-16 13:45:40 -0300

    Kubernetes: Secrets e variáveis de ambiente

--- Kubernetes/server.go
+++ Kubernetes/server.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	http.HandleFunc("/", Hello)
 	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/secret", Secret)
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -26,4 +27,10 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("failed to read file: %s", err)
 	}
 	fmt.Fprintf(w, "My family is %s", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s. Password: %s", user, password)
 }
