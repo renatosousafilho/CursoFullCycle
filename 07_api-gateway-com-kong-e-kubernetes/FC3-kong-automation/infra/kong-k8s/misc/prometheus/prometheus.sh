5f505c1acd618bc81d943f123009ff754c29bac3
Renato Filho <renatosousafilho@gmail.com>
2024-03-04 18:21:42 -0300

    API Gateway com Kong e Kubernetes: Ferramentas adicionais

--- 07_api-gateway-com-kong-e-kubernetes/FC3-kong-automation/infra/kong-k8s/misc/prometheus/prometheus.sh
+++ 07_api-gateway-com-kong-e-kubernetes/FC3-kong-automation/infra/kong-k8s/misc/prometheus/prometheus.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 kubectl create ns monitoring
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
 helm install prometheus-stack prometheus-community/kube-prometheus-stack -f prometheus.yaml --namespace monitoring
