3e2a740cb3bbeb4fc34a054cc29e37df64f16d01
Renato Filho <renatosousafilho@gmail.com>
2024-05-09 17:55:07 -0300

    DDD - Modelagem Tática e Patterns: Criando testes do Order

---
+++ ddd-modelagem-tecnica-e-patterns/projeto-com-ddd/src/entity/Order.spec.ts
@@ -0,0 +1,25 @@
+import Order from './Order';
+import OrderItem from './OrderItem';
+
+describe("Order", () => {
+  it("should thrown an error if the id is empty", () => {
+    expect(() => new Order("", "1", [])).toThrowError("ID is required");
+  });
+
+  it("should thrown an error if the customerId is empty", () => {
+    expect(() => new Order("1", "", [])).toThrowError("ID is required");
+  });
+
+  it("should throw an error if the order has no items", () => {
+    expect(() => new Order("1", "1", [])).toThrowError("An order must have at least one item");
+  });
+
+  it("should calculate the total of the order", () => {
+    const order = new Order("o1", "c1", [
+      new OrderItem("i1", "Item 1", 10),
+      new OrderItem("i2", "Item 2", 20)
+    ]);
+    const total = order.total();
+    expect(total).toBe(30);
+  });
+});
--- ddd-modelagem-tecnica-e-patterns/projeto-com-ddd/src/entity/Order.ts
+++ ddd-modelagem-tecnica-e-patterns/projeto-com-ddd/src/entity/Order.ts
@@ -1,13 +1,35 @@
 import OrderItem from './OrderItem';
 
 export default class Order {
-  _id: string;
-  _customerId: string;
-  _items: OrderItem[] = [];
+  private _id: string;
+  private _customerId: string;
+  private _items: OrderItem[] = [];
+  private _total: number = 0;
 
   constructor(id: string, customerId: string, items: OrderItem[]) {
     this._id = id;
     this._customerId = customerId;
     this._items = items;
+    this._total = this.total();
+    this.validate();
+  }
+
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error('ID is required');
+    }
+
+    if (this._customerId.length === 0) {
+      throw new Error('Customer ID is required');
+    }
+
+    // verificar se o array de items está vazio
+    if (this._items.length === 0) {
+      throw new Error('An order must have at least one item');
+    }
+  }
+
+  total(): number {
+    return this._items.reduce((acc, item) => acc + item.price, 0);
   }
 }
