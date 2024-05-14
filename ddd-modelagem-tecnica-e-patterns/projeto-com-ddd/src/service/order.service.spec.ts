d9460d5f2a6fa0a548504aa28e514afc89aac0a4
Renato Filho <renatosousafilho@gmail.com>
2024-05-14 13:48:37 -0300

    DDD - Modelagem Tática e Patterns: Definindo nosso ProductService

---
+++ ddd-modelagem-tecnica-e-patterns/projeto-com-ddd/src/service/order.service.spec.ts
@@ -0,0 +1,15 @@
+import Product from '../entity/Product';
+
+describe('Product service unit tests', () => {
+  it('should change the price of all products', () => {
+    const product1 = new Product('p1', 'Product 1', 10);
+    const product2 = new Product('p2', 'Product 2', 20);  
+
+    const products = [product1, product2];
+
+    ProductService.changePrice(products, 100);
+
+    expect(product1.price).toBe(20);
+    expect(product2.price).toBe(40);
+  });
+});
