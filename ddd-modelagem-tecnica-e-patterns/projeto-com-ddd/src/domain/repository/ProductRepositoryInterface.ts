1f6b9c8e6a861037cd69d127431e0e94d684688e
Renato Filho <renatosousafilho@gmail.com>
2024-05-15 13:49:18 -0300

    DDD - Modelagem Tática e Patterns: Definindo a camada infra

---
+++ ddd-modelagem-tecnica-e-patterns/projeto-com-ddd/src/domain/repository/ProductRepositoryInterface.ts
@@ -0,0 +1,5 @@
+import RepositoryInterface from './RepositoryInterface';
+import Product from '../entity/Product';
+
+export default interface ProductRepositoryInterface extends RepositoryInterface<Product> {
+}
