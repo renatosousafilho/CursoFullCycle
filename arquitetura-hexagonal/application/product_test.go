2a9e8ca126e7b0a59d0599f2aaf4d74137623685
Renato Filho <renatosousafilho@gmail.com>
2024-05-22 15:44:14 -0300

    Arquitetura Hexagonal na Prática: Implementando e testando as funções Disabele e IsValid

--- arquitetura-hexagonal/application/product_test.go
+++ arquitetura-hexagonal/application/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/renatosousafilho/go-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,4 +21,46 @@ func TestProduct_Enable(t *testing.T) {
 	product.Price = 0
 	err = product.Enable()
 	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Product Test"
+	product.Price = 0
+	product.Status = application.ENABLED
+
+	err := product.Disable()
+
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
+
+func Product_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Product Test"
+	product.Price = 10
+	product.Status = application.DISABLED
+
+	isValid, err := product.IsValid()
+
+	require.Nil(t, err)
+	require.True(t, isValid)
+
+	product.Status = "invalid status"
+	_, err = product.IsValid()
+
+	require.NotNil(t, err)
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater than or equal to zero", err.Error())
 }
