4a6ebaac31f9961cd27a5ddba85d0ceea8f57516
Renato Filho <renatosousafilho@gmail.com>
2024-05-23 13:37:07 -0300

    Arquitetura Hexagonal na Prática: Preprando testes do banco e criando testes do método Get de ProductDb

---
+++ arquitetura-hexagonal/adapters/db/product_test.go
@@ -0,0 +1,57 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/renatosousafilho/go-hexagonal/adapters/db"
+	"github.com/stretchr/testify/require"
+)
+
+var Db *sql.DB
+
+func setup() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(*sql.DB) {
+	createTable := `
+	CREATE TABLE products (
+		ID     string PRIMARY KEY,
+		Name   string,
+		Price  float,
+		Status string
+	);
+	`
+	stmt, err := Db.Prepare(createTable)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec()
+}
+
+func createProduct(Db *sql.DB) {
+	insertProduct := `
+	INSERT INTO products (ID, Name, Price, Status) VALUES (?, ?, ?, ?)
+	`
+	stmt, err := Db.Prepare(insertProduct)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	stmt.Exec("abc", "Product Test", 10.0, "enabled")
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setup()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "abc", product.GetID())
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 10.0, product.GetPrice())
+	require.Equal(t, "enabled", product.GetStatus())
+}
