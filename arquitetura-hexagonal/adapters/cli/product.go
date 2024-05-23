b49f4287f1f638152b01dd3cd8e39937066858ad
Renato Filho <renatosousafilho@gmail.com>
2024-05-23 14:13:50 -0300

    Arquitetura Hexagonal na Prática: Criando Adapatador CLI

---
+++ arquitetura-hexagonal/adapters/cli/product.go
@@ -0,0 +1,46 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/renatosousafilho/go-hexagonal/application"
+)
+
+func Run(service application.ProductServiceInterface, action string, productID string, productName string, productPrice float64) {
+	var result = ""
+
+	switch action {
+	case "create":
+		product, err := service.Create(productName, productPrice)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("ProductId: %s, ProductName: %s, ProductPrice: %f", 
+			product.GetID(), product.GetName(), product.GetPrice()
+		)
+	case "enable":
+		product, err := service.Get(productID)
+		if err != nil {
+			return result, err
+		}
+		res, err := service.Enable(product)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product %s has been enabled", res.GetName())
+	case "disable":
+		product, err := service.Get(productID)
+		if err != nil {
+			return result, err
+		}
+		res, err := service.Disable(product)
+		if err != nil {
+			return result, err
+		}
+		result = fmt.Sprintf("Product %s has been disabled", res.GetName())
+	default:
+		result = "Invalid action"
+	}
+
+	return result, nil
+}
