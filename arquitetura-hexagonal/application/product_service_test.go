58d4170f18d9feaeffdd4fe740d8d54e9e17e228
Renato Filho <renatosousafilho@gmail.com>
2024-05-23 10:53:03 -0300

    Arquitetura Hexagonal na Prática: Finalizando implementação do Service

--- arquitetura-hexagonal/application/product_service_test.go
+++ arquitetura-hexagonal/application/product_service_test.go
@@ -13,14 +13,10 @@ import (
 func TestProductService_Get(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-
 	product := mock_application.NewMockProductInterface(ctrl)
 	persistence := mock_application.NewMockProductPersistence(ctrl)
 	persistence.EXPECT().Get(gomock.Any()).Return(product, nil)
-
-	service := application.ProductService{
-		Persistence: persistence,
-	}
+	service := application.ProductService{Persistence: persistence}
 
 	result, err := service.Get("abc")
 	require.Nil(t, err)
@@ -30,4 +26,113 @@ func TestProductService_Get(t *testing.T) {
 	result, err = service.Get("abc")
 	require.Equal(t, errors.New("error"), err)
 	require.Nil(t, result)
+}
+
+func TestProductService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistence(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil)
+	service := application.ProductService{Persistence: persistence}
+
+	// Act
+	result, err := service.Create("Product 1", 10)
+
+	// Assert
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+	// Arrange
+	persistence.EXPECT().Save(gomock.Any()).Return(nil, errors.New("error"))
+	service = application.ProductService{Persistence: persistence}
+
+	// Act
+	result, err = service.Create("Product 1", 10)
+
+	// Assert
+	require.Equal(t, &application.Product{}, result)
+	require.Equal(t, errors.New("error"), err)
+}
+
+func TestProductService_Enable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(nil)
+	persistence := mock_application.NewMockProductPersistence(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil)
+	service := application.ProductService{Persistence: persistence}
+
+	// Act
+	result, err := service.Enable(product)
+
+	// Assert
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+	// Arrange
+	product = mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(errors.New("error"))
+
+	// Act
+	result, err = service.Enable(product)
+
+	// Assert
+	require.Equal(t, &application.Product{}, result)
+	require.Equal(t, errors.New("error"), err)
+
+	// Arrange
+	product = mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Enable().Return(nil)
+	persistence.EXPECT().Save(gomock.Any()).Return(nil, errors.New("error"))
+	service = application.ProductService{Persistence: persistence}
+
+	// Act
+	result, err = service.Enable(product)
+
+	// Assert
+	require.Equal(t, &application.Product{}, result)
+	require.Equal(t, errors.New("error"), err)
+}
+
+func TestProductService_Disable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	product := mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Disable().Return(nil)
+	persistence := mock_application.NewMockProductPersistence(ctrl)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil)
+	service := application.ProductService{Persistence: persistence}
+
+	// Act
+	result, err := service.Disable(product)
+
+	// Assert
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+	// Arrange
+	product = mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Disable().Return(errors.New("error"))
+
+	// Act
+	result, err = service.Disable(product)
+
+	// Assert
+	require.Equal(t, &application.Product{}, result)
+	require.Equal(t, errors.New("error"), err)
+
+	// Arrange
+	product = mock_application.NewMockProductInterface(ctrl)
+	product.EXPECT().Disable().Return(nil)
+	persistence.EXPECT().Save(gomock.Any()).Return(nil, errors.New("error"))
+	service = application.ProductService{Persistence: persistence}
+
+	// Act
+	result, err = service.Disable(product)
+
+	// Assert
+	require.Equal(t, &application.Product{}, result)
+	require.Equal(t, errors.New("error"), err)
 }
