ff50d382a36c704dec4dcdd4af90e5584401c102
Renato Filho <renatosousafilho@gmail.com>
2024-05-23 13:13:59 -0300

    Arquitetura Hexagonal na Prática: Inicando criação do adapter DB

---
+++ arquitetura-hexagonal/adapters/db/product.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/renatosousafilho/go-hexagonal/application"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
+	var product application.Product
+	stmt, err := p.db.Prepare("select ID, Name, Price, Status from products where ID = ?")
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	if err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
