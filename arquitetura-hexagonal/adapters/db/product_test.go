dbfb378a87755a72c1949556d96e93d79c70c1a5
Renato Filho <renatosousafilho@gmail.com>
2024-05-23 13:54:44 -0300

    Arquitetura Hexagonal na Prática: Testando método Save

--- arquitetura-hexagonal/adapters/db/product_test.go
+++ arquitetura-hexagonal/adapters/db/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/renatosousafilho/go-hexagonal/adapters/db"
+	"github.com/renatosousafilho/go-hexagonal/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -54,4 +55,28 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, "Product Test", product.GetName())
 	require.Equal(t, 10.0, product.GetPrice())
 	require.Equal(t, "enabled", product.GetStatus())
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setup()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
 }
