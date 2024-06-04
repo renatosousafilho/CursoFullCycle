f0f49c48cfa254fd478cb3204f41cd29871ecc11
Renato Filho <renatosousafilho@gmail.com>
2024-06-04 15:45:23 -0300

    DDD - Modelagem Tática e Patterns: Corrige evento utilizado no ShowConsoleLogWhenUserAddressIsUpdated

--- ddd-modelagem-tecnica-e-patterns/projeto-com-ddd/src/domain/customer/event/handler/ShowConsoleLogWhenUserAddressIsUpdated.ts
+++ ddd-modelagem-tecnica-e-patterns/projeto-com-ddd/src/domain/customer/event/handler/ShowConsoleLogWhenUserAddressIsUpdated.ts
@@ -1,7 +1,7 @@
-import CustomerCreatedEvent from '../CustomerCreatedEvent';
 import EventHandlerInterface from '../../../@shared/event/EventHandlerInterface';
-export default class ShowConsoleLogWhenUserAddressIsUpdated implements EventHandlerInterface<CustomerCreatedEvent>{
-  handle(event: CustomerCreatedEvent): void {
+import CustomerUpdatedEvent from '../CustomerUpdatedEvent';
+export default class ShowConsoleLogWhenUserAddressIsUpdated implements EventHandlerInterface<CustomerUpdatedEvent>{
+  handle(event: CustomerUpdatedEvent): void {
     console.log(`Endereço do cliente ${event.eventData.name} foi atualizado para ${event.eventData.address}`);
   }
 }
